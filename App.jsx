9bb7dd4b0bfe278f04275e88cc3c08de3fa533b3
KazMat1 <90205997+KazMat1@users.noreply.github.com>
2022-04-12 17:53:48 +0900

    Style memo screen

--- App.jsx
+++ App.jsx
@@ -1,17 +1,124 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Hello from './src/components/Hello';
 
 export default function App() {
   return (
     // HTML
     <View style={styles.container}>
-      <Hello bang>World</Hello>
-      <Hello style={{ fontSize: 16 }}>Small World</Hello>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* eslint-disable-next-line */}
-      <StatusBar style="auto" />
+      {/* App bar */}
+      <View style={styles.appbar}>
+        <View style={styles.appbarInner}>
+          <Text style={styles.appbarTitle}>MemoApp</Text>
+          <Text style={styles.appbarRight}>ログアウト</Text>
+        </View>
+      </View>
+      {/* Memo list */}
+      <View>
+        {/* Memo list item */}
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+        {/* Memo list item */}
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+        {/* Memo list item */}
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+        {/* Memo list item */}
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+        {/* Memo list item */}
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+        {/* Memo list item */}
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+        {/* Memo list item */}
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+        {/* Memo list item */}
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+        {/* Memo list item */}
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+        {/* Memo list item */}
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+      </View>
+      {/* Create button */}
+      <View style={styles.circleButton}>
+        <Text style={styles.plusButton}>+</Text>
+      </View>
     </View>
   );
 }
@@ -20,8 +127,70 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F0F4F8',
+  },
+  appbar: {
+    width: '100%',
+    height: 104,
+    backgroundColor: '#467FD3',
+    justifyContent: 'flex-end',
+  },
+  appbarInner: {
     alignItems: 'center',
+  },
+  appbarRight: {
+    position: 'absolute',
+    right: 19,
+    bottom: 9,
+    fontSize: 12,
+    lineHeight: 24,
+    color: 'rgba(255, 255, 255, 0.8)',
+  },
+  appbarTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  memoListItem: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.15)',
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484',
+  },
+  circleButton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
     justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 40,
+    right: 40,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  plusButton: {
+    fontSize: 40,
+    lineHeight: 40,
+    color: '#fff',
   },
 });
