58a3b119da693cc02f929f92c6e19d13d1d76b6e
KazMat1 <90205997+KazMat1@users.noreply.github.com>
2022-04-12 18:40:41 +0900

    Extract components

--- App.jsx
+++ App.jsx
@@ -1,124 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import AppBar from './src/components/AppBar';
+import MemoList from './src/components/MemoList';
+import CircleButton from './src/components/CircleButton';
 
 export default function App() {
   return (
     // HTML
     <View style={styles.container}>
-      {/* App bar */}
-      <View style={styles.appbar}>
-        <View style={styles.appbarInner}>
-          <Text style={styles.appbarTitle}>MemoApp</Text>
-          <Text style={styles.appbarRight}>ログアウト</Text>
-        </View>
-      </View>
+      <AppBar />
       {/* Memo list */}
-      <View>
-        {/* Memo list item */}
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
-          </View>
-          <View>
-            <Text>×</Text>
-          </View>
-        </View>
-        {/* Memo list item */}
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
-          </View>
-          <View>
-            <Text>×</Text>
-          </View>
-        </View>
-        {/* Memo list item */}
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
-          </View>
-          <View>
-            <Text>×</Text>
-          </View>
-        </View>
-        {/* Memo list item */}
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
-          </View>
-          <View>
-            <Text>×</Text>
-          </View>
-        </View>
-        {/* Memo list item */}
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
-          </View>
-          <View>
-            <Text>×</Text>
-          </View>
-        </View>
-        {/* Memo list item */}
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
-          </View>
-          <View>
-            <Text>×</Text>
-          </View>
-        </View>
-        {/* Memo list item */}
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
-          </View>
-          <View>
-            <Text>×</Text>
-          </View>
-        </View>
-        {/* Memo list item */}
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
-          </View>
-          <View>
-            <Text>×</Text>
-          </View>
-        </View>
-        {/* Memo list item */}
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
-          </View>
-          <View>
-            <Text>×</Text>
-          </View>
-        </View>
-        {/* Memo list item */}
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2022年4月12日 10:00</Text>
-          </View>
-          <View>
-            <Text>×</Text>
-          </View>
-        </View>
-      </View>
-      {/* Create button */}
-      <View style={styles.circleButton}>
-        <Text style={styles.plusButton}>+</Text>
-      </View>
+      <MemoList />
+      <MemoList />
+      <MemoList />
+      {/* Circle button */}
+      <CircleButton>+</CircleButton>
     </View>
   );
 }
@@ -128,69 +25,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F0F4F8',
-  },
-  appbar: {
-    width: '100%',
-    height: 104,
-    backgroundColor: '#467FD3',
-    justifyContent: 'flex-end',
-  },
-  appbarInner: {
-    alignItems: 'center',
-  },
-  appbarRight: {
-    position: 'absolute',
-    right: 19,
-    bottom: 9,
-    fontSize: 12,
-    lineHeight: 24,
-    color: 'rgba(255, 255, 255, 0.8)',
-  },
-  appbarTitle: {
-    marginBottom: 8,
-    fontSize: 22,
-    lineHeight: 32,
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-  memoListItem: {
-    backgroundColor: '#fff',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingVertical: 16,
-    paddingHorizontal: 19,
-    borderBottomWidth: 1,
-    borderColor: 'rgba(0, 0, 0, 0.15)',
-  },
-  memoListItemTitle: {
-    fontSize: 16,
-    lineHeight: 32,
-  },
-  memoListItemDate: {
-    fontSize: 12,
-    lineHeight: 16,
-    color: '#848484',
-  },
-  circleButton: {
-    backgroundColor: '#467FD3',
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    bottom: 40,
-    right: 40,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 8 },
-    shadowOpacity: 0.25,
-    shadowRadius: 8,
-    elevation: 8,
-  },
-  plusButton: {
-    fontSize: 40,
-    lineHeight: 40,
-    color: '#fff',
   },
 });
---
+++ src/components/CircleButton.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { string } from 'prop-types';
+
+export default function CircleButton(props) {
+  const { children } = props;
+  return (
+    <View style={styles.circleButton}>
+      <Text style={styles.plusButton}>{children}</Text>
+    </View>
+  );
+}
+
+CircleButton.propTypes = {
+  children: string.isRequired,
+};
+
+const styles = StyleSheet.create({
+  circleButton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 40,
+    right: 40,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  plusButton: {
+    fontSize: 40,
+    lineHeight: 40,
+    color: '#fff',
+  },
+});
