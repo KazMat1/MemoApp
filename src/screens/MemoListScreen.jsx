701c7de205d635a7acf8cbd6eec412435814f8f1
KazMat1 <90205997+KazMat1@users.noreply.github.com>
2022-04-22 01:17:51 +0900

    Create log out button and set up the navigation

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import MemoList from '../components/MemoList';
 import CircleButton from '../components/CircleButton';
+import LogOutButton from '../components/LogOutButton';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
+  useEffect(() => {
+    // AppBarのnavigationを使っている時、コンポーネントを追加する方法
+    navigation.setOptions({
+      // eslint-disable-next-line react/no-unstable-nested-components
+      headerRight: () => <LogOutButton />,
+    });
+  }, []);
   return (
-    // HTML
     <View style={styles.container}>
-      {/* Memo list */}
       <MemoList />
       <MemoList />
       <MemoList />
-      {/* Circle button */}
       <CircleButton
         name="plus"
         onPress={() => { navigation.navigate('MemoCreate'); }}
